b6bc3e18a38bb2f20fdcf14ba6c76c45a71abee4
Rakha-arch <miftahuzahran@gmail.com>
2026-07-22 14:52:35 +0700

    FUNGSI UTAMA SERVER

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const PORT = 3000;
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
+
+async function startServer() {
+    await connectDatabase(); 
+    app.listen(PORT, () => { 
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
