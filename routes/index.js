534258e33791ca6437e7680fcc09a4e3ec410641
Rakha-arch <miftahuzahran@gmail.com>
2026-07-22 15:03:47 +0700

    definisi routed

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'API Architecture Meeting 6 is running'
+  });
+})
